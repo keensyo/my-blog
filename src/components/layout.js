eae03637e5ba76498f3f46a8906ad6037d2d0eca
keensyo <k66mdg55@gmail.com>
2021-01-31 23:00:12 +0900

    change title, bio

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,14 +20,13 @@ const Layout = ({ location, title, children }) => {
     )
   }
 
+// TODO：footerをつくる
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+          
       </footer>
     </div>
   )
